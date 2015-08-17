0670b58002412572f6dd171389aebd2749e5f919
Ferran Vilà Conesa <ikocel1@gmail.com>
2015-08-17 11:44:01 +0200

    MetricServerRepository

--- src/main/java/me/adaptive/core/data/repo/MetricServerRepository.java
+++ src/main/java/me/adaptive/core/data/repo/MetricServerRepository.java
@@ -17,9 +17,13 @@
 package me.adaptive.core.data.repo;
 
 import me.adaptive.core.data.domain.MetricServerEntity;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Set;
 
 /**
  * Server information metrics repository
@@ -28,5 +32,19 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface MetricServerRepository extends JpaRepository<MetricServerEntity, Long>, JpaSpecificationExecutor<MetricServerEntity> {
+
+    /**
+     select ms.created_at, msa.value from metrics_server ms
+     join metrics_server_attributes msa on ms.id=msa.id_event
+     where ms.hostname = 'my.adaptive.me'
+     and msa.name = 'disk_used'
+     order by ms.created_at desc
+     limit 20;
+     */
+
+    // @Query("select ms, msa from MetricServerEntity ms inner join ms.attributes msa where ms.hostname = :hostname and msa.name = :metric")
+    // @Query("select ms from MetricServerEntity ms where ms.hostname = ?1 and ms.id = (select msa from ms.attributes msa where msa.name = ?2)")
+    @Query("select ms from MetricServerEntity ms join ms.attributes msa")
+    Set<MetricServerEntity> getMetricsHostnameAndMetrics(String hostname, String metric, PageRequest pageRequest);
 
 }
